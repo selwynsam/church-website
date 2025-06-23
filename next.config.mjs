f5253bb126c5f230022669970f209b144c23ea54
selwynsam <selwynsam13@gmail.com>
2025-06-23 22:38:55 +0530

    chore: nextjs version 15.3.4 setup with tailwind

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+};
+
+export default nextConfig;
